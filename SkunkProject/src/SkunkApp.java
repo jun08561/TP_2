2494d7e9f761d56262c1af7c325593010e32cab7
June <June@10.66.21.133>
2018-10-12 20:01:31 -0500

    first commit

---
+++ SkunkProject/src/SkunkApp.java
@@ -0,0 +1,19 @@
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+public class SkunkApp
+{
+
+	public static void main(String[] args)
+	{
+		StdOut.println("Welcome to 635 Skunk project!");
+		
+		StdOut.println("Play one Turn for player1");
+		
+		String player = "player1";
+		
+		Turn turn = new Turn(); 
+		
+	}
+
+}
